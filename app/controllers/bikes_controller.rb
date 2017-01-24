9dfeafb078470b4c3be877c4e0f742051f10a368
David O'Leary <david.oleary174@gmail.com>
2017-01-24 16:25:41 +0000

    db working

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,9 +1,8 @@
 class BikesController < ApplicationController
   
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def index
-    bikes = current_user.bikes
 
     render json: bikes
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get 'bikes' => 'bikes#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
